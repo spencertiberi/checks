a5548fb7b3d567e06f95f9ec281dcc8c9514a835
Spencer Tiberi <spencertiberi@gmail.com>
2018-01-13 17:35:10 -0500

    add challenge0

--- compsci.one/calc/check50/__init__.py
+++ compsci.one/calc/check50/__init__.py
@@ -42,3 +42,10 @@ class Credit(Checks):
         self.spawn("python calc.py").stdin("2")\
 		.stdin("^")\
 		.stdin("4").stdout("16.0\n", "16.0\n").exit(0)
+
+    @check("exists")
+    def test6(self):
+        """informs user of "invalid opperation""""
+        self.spawn("python calc.py").stdin("2")\
+		.stdin("&")\
+		.stdin("4").stdout("invalid opperation\n", "invalid opperation\n").exit(0)
