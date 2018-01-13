3600971a0dadfc90574c810f91aa157ab185086b
Spencer Tiberi <spencertiberi@gmail.com>
2018-01-13 10:52:37 -0500

    add challenge0

--- compsci.one/challenge0/check50/__init__.py
+++ compsci.one/challenge0/check50/__init__.py
@@ -14,4 +14,4 @@ class Challenge(Checks):
 	@check("exists0")
 	def test_add(self):
 		"""input of '2', '+', and '2' yields output of 4"""
-		self.spawn("python calculator.py").stdin("2").stdin("+")..stdin("2")stdout("4\n", "4\n").exit(0)
+		self.spawn("python calculator.py").stdin("2").stdin("+").stdin("2")stdout("4\n", "4\n").exit(0)
