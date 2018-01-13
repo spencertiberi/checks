8dc9fd9253c3348c6d4171b2cfb7d4cb77e880c7
Spencer Tiberi <spencertiberi@gmail.com>
2018-01-13 17:25:51 -0500

    add challenge0

--- compsci.one/calc/check50/__init__.py
+++ compsci.one/calc/check50/__init__.py
@@ -21,3 +21,10 @@ class Credit(Checks):
         self.spawn("python calc.py").stdin("2")\
 		.stdin("-")\
 		.stdin("2").stdout("0.0\n", "0.0\n").exit(0)
+
+    @check("exists")
+    def test3(self):
+        """input of 2, x, and 2 yields 4.0"""
+        self.spawn("python calc.py").stdin("2")\
+		.stdin("x")\
+		.stdin("2").stdout("4.0\n", "4.0\n").exit(0)
