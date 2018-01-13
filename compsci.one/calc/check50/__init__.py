08ad21025d029e1945095ef7d26703f7b99a8f4d
Spencer Tiberi <spencertiberi@gmail.com>
2018-01-13 17:14:56 -0500

    add challenge0

--- compsci.one/calc/check50/__init__.py
+++ compsci.one/calc/check50/__init__.py
@@ -10,47 +10,42 @@ class Credit(Checks):
 
     @check("exists")
     def test1(self):
-        """input of 2, +, and 2 yields 4"""
+        """input of 2, +, and 2 yields 4.0"""
         self.spawn("python calc.py").stdin("2")\
 		.stdin("+")\
 		.stdin("2").stdout("4.0\n", "4.0\n").exit(0)
 
-    @check("exists")
+	@check("exists")
     def test2(self):
-        """identifies 371449635398431 as AMEX"""
-        self.spawn("python calc.py").stdin("371449635398431").stdout("^AMEX\n", "AMEX\n").exit(0)
+        """input of 2, -, and 2 yields 0.0"""
+        self.spawn("python calc.py").stdin("2")\
+		.stdin("-")\
+		.stdin("2").stdout("0.0\n", "0.0\n").exit(0)
 
     @check("exists")
     def test3(self):
-        """identifies 5555555555554444 as MASTERCARD"""
-        self.spawn("python credit.py").stdin("5555555555554444").stdout("^MASTERCARD\n", "MASTERCARD\n").exit(0)
+        """input of 2, x, and 3 yields 6.0"""
+        self.spawn("python calc.py").stdin("2")\
+		.stdin("x")\
+		.stdin("3").stdout("6.0\n", "6.0\n").exit(0)
 
     @check("exists")
     def test4(self):
-        """identifies 5105105105105100 as MASTERCARD"""
-        self.spawn("python credit.py").stdin("5105105105105100").stdout("^MASTERCARD\n", "MASTERCARD\n").exit(0)
+        """input of 4, /, and 2 yields 2.0"""
+        self.spawn("python calc.py").stdin("4")\
+		.stdin("/")\
+		.stdin("2").stdout("2.0\n", "2.0\n").exit(0)
 
     @check("exists")
     def test5(self):
-        """identifies 4111111111111111 as VISA"""
-        self.spawn("python credit.py").stdin("4111111111111111").stdout("^VISA\n", "VISA\n").exit(0)
+        """input of 3, ^, and 2 yields 9.0"""
+        self.spawn("python calc.py").stdin("3")\
+		.stdin("^")\
+		.stdin("2").stdout("9.0\n", "9.0\n").exit(0)
 
     @check("exists")
     def test6(self):
-        """identifies 4012888888881881 as VISA"""
-        self.spawn("python credit.py").stdin("4012888888881881").stdout("^VISA\n", "VISA\n").exit(0)
-
-    @check("exists")
-    def test7(self):
-        """identifies 1234567890 as INVALID"""
-        self.spawn("python credit.py").stdin("1234567890").stdout("^INVALID\n", "INVALID\n").exit(0)
-
-    @check("exists")
-    def test_reject_foo(self):
-        """rejects a non-numeric input of "foo" """
-        self.spawn("python credit.py").stdin("foo").reject()
-
-    @check("exists")
-    def test_reject_empty(self):
-        """rejects a non-numeric input of "" """
-        self.spawn("python credit.py").stdin("").reject()
+        """input of 3, /, and 2 yields 1.5"""
+        self.spawn("python calc.py").stdin("3")\
+		.stdin("/")\
+		.stdin("2").stdout("1.5\n", "1.5\n").exit(0)
