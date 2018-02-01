01efc7abb1b7bd74578d4217c25aa57a55f3fd9d
Spencer Tiberi <spencertiberi@gmail.com>
2018-02-01 11:27:06 -0500

    checks

--- compsci.one/python0/spaghetti/check50/__init__.py
+++ compsci.one/python0/spaghetti/check50/__init__.py
@@ -5,44 +5,14 @@ import shlex
 from check50 import *
 
 
-class Pyhton0(Checks):
+class Spaghetti(Checks):
 
     @check()
     def exists(self):
-        """hello.py exists."""
-        self.require("hello.py")
+        """spaghetti.py exists."""
+        self.require("spaghetti.py")
 
     @check("exists")
     def test1(self):
-        """input of 2, +, and 4 yields 6.0"""
-        self.spawn("python calc.py").stdin("2")\
-		.stdin("+")\
-		.stdin("4").stdout("6.0\n", "6.0\n").exit(0)
-
-    @check("exists")
-    def test2(self):
-        """input of 2, -, and 4 yields -2.0"""
-        self.spawn("python calc.py").stdin("2")\
-		.stdin("-")\
-		.stdin("4").stdout("-2.0\n", "-2.0\n").exit(0)
-
-    @check("exists")
-    def test3(self):
-        """input of 2, x, and 4 yields 8.0"""
-        self.spawn("python calc.py").stdin("2")\
-		.stdin("x")\
-		.stdin("4").stdout("8.0\n", "8.0\n").exit(0)
-
-    @check("exists")
-    def test4(self):
-        """input of 2, /, and 4 yields 0.5"""
-        self.spawn("python calc.py").stdin("2")\
-		.stdin("/")\
-		.stdin("4").stdout("0.5\n", "0.5\n").exit(0)
-
-    @check("exists")
-    def test5(self):
-        """input of 2, ^, and 4 yields 16.0"""
-        self.spawn("python calc.py").stdin("2")\
-		.stdin("^")\
-		.stdin("4").stdout("16.0\n", "16.0\n").exit(0)
+        """There's vomit on his sweater already!"""
+        self.spawn("python spaghetti.py").stdout("There's vomit on his sweater already\nMom's spaghetti")
