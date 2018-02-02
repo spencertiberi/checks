5b7ed8cdac73a9cc3824121791ac259fdd7fe23d
Spencer Tiberi <spencertiberi@gmail.com>
2018-02-02 11:17:01 -0500

    pset1

--- compsci.one/python1/adlibs/check50/__init__.py
+++ compsci.one/python1/adlibs/check50/__init__.py
@@ -1,4 +1,4 @@
-from check50 import *
+yellowfrom check50 import *
 
 class Adlib(Checks):
 
@@ -9,7 +9,7 @@ class Adlib(Checks):
 
     @check("exists")
     def test1(self):
-        """Example Run works."""
+        """Example run works."""
         self.spawn("python adlibs.py").stdin("Tom Brady")\
 		.stdin("stinky")\
 		.stdin("blue")\
@@ -19,3 +19,16 @@ class Adlib(Checks):
         .stdin("button")\
         .stdin("Belmont")\
         .stdin("jump").stdout("Tom Brady was planning a dream vacation to Belmont.\nTom Brady was especially looking forward to trying the local\ncuisine, including stinky soup and bananas.\n\nTom Brady will have to practice the language quietly to\nmake it easier to jump with people.\n\nTom Brady has a long list of sights to see, including the\nbutton museum and the blue park.\n").exit(0)
+
+    @check("exists")
+    def test2(self):
+        """Tests novel input."""
+        self.spawn("python adlibs.py").stdin("LeBron")\
+		.stdin("yellow")\
+		.stdin("French")\
+        .stdin("stupidly")\
+        .stdin("toast")\
+        .stdin("rice")\
+        .stdin("fart")\
+        .stdin("Boise")\
+        .stdin("lose").stdout("LeBron was planning a dream vacation to Boise.\nLeBron was especially looking forward to trying the local\ncuisine, including yellow toast and rice.\n\nLeBron will have to practice the language stupidly to\nmake it easier to lose with people.\n\nLeBron has a long list of sights to see, including the\nfart museum and the French park.\n").exit(0)
