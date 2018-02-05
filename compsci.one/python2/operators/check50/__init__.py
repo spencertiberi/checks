79f4c9a5ff96d36643a6f9c63b51ca431f8af15f
Spencer Tiberi <spencertiberi@gmail.com>
2018-02-05 09:43:05 -0500

    add pset2

--- compsci.one/python2/operators/check50/__init__.py
+++ compsci.one/python2/operators/check50/__init__.py
@@ -11,10 +11,10 @@ class Op(Checks):
     def test1(self):
         """Input of 7 and 2"""
         self.spawn("python operators.py").stdin("7")\
-		.stdin("2").stdout("Sum: 9\nDifference: 5\n Product: 14\nQuotient: 3.5\nRemainder: 1").exit(0)
+		.stdin("2").stdout("Sum: 9\nDifference: 5\nProduct: 14\nQuotient: 3.5\nRemainder: 1").exit(0)
 
     @check("exists")
     def test2(self):
         """Input of 5 and 4"""
         self.spawn("python operators.py").stdin("7")\
-		.stdin("4").stdout("Sum: 11\nDifference: 3\n Product: 28\nQuotient: 1.75\nRemainder: 3").exit(0)
+		.stdin("4").stdout("Sum: 11\nDifference: 3\nProduct: 28\nQuotient: 1.75\nRemainder: 3").exit(0)
