6f5db965b6cea0b21f82b7a3c44937892d2d5414
Spencer Tiberi <spencertiberi@gmail.com>
2018-02-05 08:58:57 -0500

    add pset2

--- compsci.one/python2/hour/check50/__init__.py
+++ compsci.one/python2/hour/check50/__init__.py
@@ -1,13 +1,18 @@
 from check50 import *
 
-class Eight(Checks):
+class hour(Checks):
 
     @check()
     def exists(self):
-        """eight.py exists."""
-        self.require("eight.py")
+        """hour.py exists."""
+        self.require("hour.py")
 
     @check("exists")
     def test1(self):
-        """Prints 80 when 10 is entered."""
-        self.spawn("python eight.py").stdin("10").stdout("80\n", "80\n").exit(0)
+        """Prints 2 with input of 9."""
+        self.spawn("python hour.py").stdin("9").stdout("In 5 hours it will be 2 o'clock\n", "In 5 hours it will be 2 o'clock\n\n").exit(0)
+
+    @check("exists")
+    def test2(self):
+        """Prints 11 with input of 6."""
+        self.spawn("python hour.py").stdin("6").stdout("In 5 hours it will be 11 o'clock\n", "In 5 hours it will be 11 o'clock\n\n").exit(0)
--- compsci.one/python2/operators/check50/__init__.py
+++ compsci.one/python2/operators/check50/__init__.py
@@ -1,34 +1,20 @@
 from check50 import *
 
-class Adlib(Checks):
+class Op(Checks):
 
     @check()
     def exists(self):
-        """adlibs.py exists."""
-        self.require("adlibs.py")
+        """operators.py exists."""
+        self.require("operators.py")
 
     @check("exists")
     def test1(self):
-        """Example run works."""
-        self.spawn("python adlibs.py").stdin("Tom Brady")\
-		.stdin("stinky")\
-		.stdin("blue")\
-        .stdin("quietly")\
-        .stdin("soup")\
-        .stdin("bananas")\
-        .stdin("button")\
-        .stdin("Belmont")\
-        .stdin("jump").stdout("Tom Brady was planning a dream vacation to Belmont.\nTom Brady was especially looking forward to trying the local\ncuisine, including stinky soup and bananas.\n\nTom Brady will have to practice the language quietly to\nmake it easier to jump with people.\n\nTom Brady has a long list of sights to see, including the\nbutton museum and the blue park.\n").exit(0)
+        """Input of 7 and 2"""
+        self.spawn("python operators.py").stdin("7")\
+		.stdin("2").stdout("Sum: 9\nDifference: 5\n Product: 14\nQuotient: 3.5\nRemainder: 1").exit(0)
 
     @check("exists")
     def test2(self):
-        """Tests novel input."""
-        self.spawn("python adlibs.py").stdin("LeBron")\
-		.stdin("yellow")\
-		.stdin("French")\
-        .stdin("stupidly")\
-        .stdin("toast")\
-        .stdin("rice")\
-        .stdin("fart")\
-        .stdin("Boise")\
-        .stdin("lose").stdout("LeBron was planning a dream vacation to Boise.\nLeBron was especially looking forward to trying the local\ncuisine, including yellow toast and rice.\n\nLeBron will have to practice the language stupidly to\nmake it easier to lose with people.\n\nLeBron has a long list of sights to see, including the\nfart museum and the French park.\n").exit(0)
+        """Input of 5 and 4"""
+        self.spawn("python operators.py").stdin("7")\
+		.stdin("4").stdout("Sum: 11\nDifference: 3\n Product: 28\nQuotient: 1.75\nRemainder: 3").exit(0)
