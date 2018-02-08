f7bcf94ebbdb1d22564b9b66da45234b9dc4cc7e
Spencer Tiberi <spencertiberi@gmail.com>
2018-02-08 11:22:54 -0500

    pset3

--- compsci.one/python3/hour2/check50/__init__.py
+++ compsci.one/python3/hour2/check50/__init__.py
@@ -11,10 +11,10 @@ class Hour2(Checks):
     def test1(self):
         """Prints 3 with input of 9 and 6."""
         self.spawn("python hour2.py").stdin("9")\
-        .stdin("6").stdout("In 7 hours it will be 3 o'clock\n", "In 5 hours it will be 2 o'clock\n").exit(0)
+        .stdin("6").stdout("In 6 hours it will be 3 o'clock\n", "In 6 hours it will be 3 o'clock\n").exit(0)
 
     @check("exists")
     def test2(self):
         """Prints 12 with input of 5 and 7."""
         self.spawn("python hour2.py").stdin("5")\
-        .stdin("7").stdout("In 7 hours it will be 12 o'clock\n", "In 5 hours it will be 2 o'clock\n").exit(0)
+        .stdin("7").stdout("In 7 hours it will be 12 o'clock\n", "In 7 hours it will be 12 o'clock\n").exit(0)
