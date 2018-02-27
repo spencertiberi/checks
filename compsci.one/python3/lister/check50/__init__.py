2dca177709ee1b63f4f4c2c492c9890f29f93c56
Spencer Tiberi <spencertiberi@gmail.com>
2018-02-26 19:51:24 -0500

    pset4

--- compsci.one/python3/lister/check50/__init__.py
+++ compsci.one/python3/lister/check50/__init__.py
@@ -11,15 +11,15 @@ class Lister(Checks):
     def test1(self):
         """Input of 3, "pizza", "cheese", "pineapple" works"""
         self.spawn("python lister.py").stdin("3")\
-		.stdin("pizza")\
-        .stdin("cheese")\
-        .stdin("pineapple").stdout("['pizza', 'cheese', 'pineapple']\n").exit(0)
+		.stdin('pizza')\
+        .stdin('cheese')\
+        .stdin('pineapple').stdout("['pizza', 'cheese', 'pineapple']\n").exit(0)
 
     @check("exists")
     def test2(self):
         """Input of 4, "pizza", "cheese", "pineapple", "anchovies" works"""
-        self.spawn("python lister.py").stdin("3")\
-		.stdin("pizza")\
-        .stdin("cheese")\
-        .stdin("pineapple")\
-        .stdin("anchovies").stdout("['pizza', 'cheese', 'pineapple', 'anchovies']\n").exit(0)
+        self.spawn("python lister.py").stdin("4")\
+		.stdin('pizza')\
+        .stdin('cheese')\
+        .stdin('pineapple')\
+        .stdin('anchovies').stdout("['pizza', 'cheese', 'pineapple', 'anchovies']\n").exit(0)
